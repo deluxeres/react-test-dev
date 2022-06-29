7fcf10316370fc13f10492fe35bcae0d99c40c1a
Ruslan <88685139+deluxeres@users.noreply.github.com>
2022-06-29 20:07:36 +0300

    api request

--- deluxeLibary.jsx
+++ deluxeLibary.jsx
@@ -6,3 +6,24 @@
   })}
 
 
+
+
+//Вывод обьектов по API запросу
+  const [post, setPost] = useState([]);
+
+  React.useEffect(() => {
+    fetch("https://62b9d604ff109cd1dc9c3cc3.mockapi.io/table")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setPost(json);
+      });
+  }, []);
+// + к этому проходимся по масиму с помощью метода map 
+{post.map((obj) => {
+    return (
+   );
+})}
+//Вывод обьектов по API запросу
+
